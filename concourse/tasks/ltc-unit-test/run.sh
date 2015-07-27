216fc29e34a35f964d7b0be72d535f265f129a9e
David Wadden <dwadden@pivotal.io>
2015-07-27 16:35:00 -0500

    wip

--- concourse/tasks/ltc-unit-test/run.sh
+++ concourse/tasks/ltc-unit-test/run.sh
@@ -4,9 +4,9 @@ set -x -e
 
 export LATTICE_SRC_PATH=$PWD
 export DIEGO_RELEASE_PATH=$PWD/lattice/build/diego-release
-
-
 export GOPATH=$PWD/lattice/build/diego-release
+
+rm -rf $GOPATH/src/github.com/docker/docker
 
 go get github.com/onsi/ginkgo/ginkgo
 
