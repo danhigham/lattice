f7a1190c47cfcda380178270c521fd92199f0349
David Wadden <dwadden@pivotal.io>
2015-07-27 15:54:50 -0500

    wip - run ltc unit tests job

--- concourse/tasks/ltc-unit-test/run.sh
+++ concourse/tasks/ltc-unit-test/run.sh
@@ -6,3 +6,14 @@ export LATTICE_SRC_PATH=$PWD
 export DIEGO_RELEASE_PATH=$PWD/lattice/build/diego-release
 
 
+export GOPATH=$PWD/lattice/build/diego-release
+
+go get github.com/onsi/ginkgo/ginkgo
+go get github.com/dajulia3/godep
+
+pushd lattice/ltc
+	godep restore
+
+	./scripts/test
+popd
+
