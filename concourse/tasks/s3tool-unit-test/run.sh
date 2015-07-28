c94448206cc2bd460d62bc14cd0b953d1c22df3d
David Wadden <dwadden@pivotal.io>
2015-07-28 15:41:02 -0500

    wip - add s3tool to lattice pipeline

---
+++ concourse/tasks/s3tool-unit-test/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+set -x -e
+
+export LATTICE_SRC_PATH=$PWD/lattice
+export GOPATH=$PWD/go
+export PATH=$GOPATH/bin:$PATH
+
+go get github.com/onsi/ginkgo/ginkgo
+
+mkdir -p $GOPATH/src/github.com/cloudfoundry-incubator/lattice/cell-helpers
+pushd $GOPATH/src/github.com/cloudfoundry-incubator/lattice/cell-helpers
+	ln -sfv $LATTICE_SRC_PATH/cell-helpers/s3tool s3tool
+
+	pushd s3tool
+		go get ./...
+		
+		ginkgo -r -race
+	popd
+popd
+
