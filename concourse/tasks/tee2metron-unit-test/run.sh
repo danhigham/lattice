562c04b2575f3c39c98f6b762ebd0203b12a3487
David Wadden <dwadden@pivotal.io>
2015-07-28 16:10:28 -0500

    wip - add tee2metron unit test job

---
+++ concourse/tasks/tee2metron-unit-test/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+set -x -e
+
+export LATTICE_SRC_PATH=$PWD/lattice
+export GOPATH=$PWD/go
+export PATH=$GOPATH/bin:$PATH
+
+go get github.com/onsi/ginkgo/ginkgo
+go get github.com/onsi/gomega
+
+mkdir -p $GOPATH/src/github.com/cloudfoundry-incubator/lattice/cell-helpers
+pushd $GOPATH/src/github.com/cloudfoundry-incubator/lattice/cell-helpers
+	ln -sfv $LATTICE_SRC_PATH/cell-helpers/tee2metron tee2metron
+
+	pushd tee2metron
+		go get -v ./...
+		
+		ginkgo -r -race
+	popd
+popd
+
