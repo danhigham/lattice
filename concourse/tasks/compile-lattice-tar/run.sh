66cad9091f9c4523604501c75cbc844982a4b892
David Wadden <dwadden@pivotal.io>
2015-07-29 09:28:20 -0500

    wip - remove comments

--- concourse/tasks/compile-lattice-tar/run.sh
+++ concourse/tasks/compile-lattice-tar/run.sh
@@ -7,15 +7,6 @@ export DIEGO_RELEASE_PATH=$PWD/lattice/build/diego-release
 export GOPATH=$DIEGO_RELEASE_PATH
 export PATH=$GOPATH/bin:$PATH
 
-## should not need this
-# rm -rf $GOPATH/src/github.com/docker/docker
-
-## should not need this
-# pushd $GOPATH/src/github.com/cloudfoundry-incubator
-# 	## make me relative
-# 	ln -sfv $LATTICE_SRC_PATH lattice
-# popd
-
 $LATTICE_SRC_PATH/pipeline/01_compilation/compile_lattice_tar
 
 
