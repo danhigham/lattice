8734e3303afde02bbebf2c45cea5900c5fbdcaa6
Chris Maher and David Dening <pair+cmaher+ddening@pivotallabs.com>
2015-07-29 17:01:40 -0400

    wip56

--- concourse/tasks/compile-ltc/run.sh
+++ concourse/tasks/compile-ltc/run.sh
@@ -10,7 +10,7 @@ ln -sf $LATTICE_SRC_PATH $PWD/go/src/github.com/cloudfoundry-incubator/lattice
 export LATTICE_VERSION=$(git -C $LATTICE_SRC_PATH describe)
 export DIEGO_VERSION=$(cat $LATTICE_SRC_PATH/DIEGO_VERSION)
 
-export GOPATH=$LATTICE_SRC_PATH/Godeps/_workspace:$PWD/go
+export GOPATH=$LATTICE_SRC_PATH/ltc/Godeps/_workspace:$PWD/go
 
 GOARCH=amd64 GOOS=linux go build \
     -ldflags \
--- concourse/tasks/ltc-unit-test/run.sh
+++ concourse/tasks/ltc-unit-test/run.sh
@@ -8,7 +8,7 @@ mkdir -p $PWD/go/src/github.com/cloudfoundry-incubator $PWD/go/bin
 ln -sf $LATTICE_SRC_PATH $PWD/go/src/github.com/cloudfoundry-incubator/lattice
 
 export GOBIN=$PWD/go/bin
-export GOPATH=$LATTICE_SRC_PATH/Godeps/_workspace:$PWD/go
+export GOPATH=$LATTICE_SRC_PATH/ltc/Godeps/_workspace:$PWD/go
 export PATH=$GOBIN:$PATH
 
 go install github.com/onsi/ginkgo/ginkgo
