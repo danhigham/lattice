7c6f6844a11937947320232ddbf2628556788af4
Chris Maher and David Dening <pair+cmaher+ddening@pivotallabs.com>
2015-07-29 15:12:00 -0400

    wip

--- concourse/tasks/compile-lattice-tar/run.sh
+++ concourse/tasks/compile-lattice-tar/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 set -x -e
 
@@ -7,23 +7,33 @@ export DIEGO_RELEASE_PATH=$PWD/lattice/build/diego-release
 export GOPATH=$DIEGO_RELEASE_PATH
 export PATH=$GOPATH/bin:$PATH
 
+export LATTICE_VERSION=$(git -C $LATTICE_SRC_PATH describe)
+export DIEGO_VERSION=$(cat $LATTICE_SRC_PATH/DIEGO_VERSION)
+export CF_VERSION=$(cat $LATTICE_SRC_PATH/CF_VERSION)
+
 pushd $DIEGO_RELEASE_PATH
-	git checkout $(cat $LATTICE_SRC_PATH/DIEGO_VERSION)
+	git checkout $DIEGO_VERSION
 	git clean -xffd
 	./scripts/update
 popd
 
 pushd $LATTICE_SRC_PATH/build/cf-release
-	git checkout $(cat $LATTICE_SRC_PATH/CF_VERSION)
+	git checkout $CF_VERSION
 	git clean -xffd
 	./update
 popd
 
-$LATTICE_SRC_PATH/pipeline/01_compilation/compile_lattice_tar
+$LATTICE_SRC_PATH/cluster/scripts/compile \
+    $LATTICE_SRC_PATH/build/lattice-build \
+    $LATTICE_SRC_PATH/build/diego-release \
+    $LATTICE_SRC_PATH/build/cf-release \
+    $LATTICE_SRC_PATH
 
-pushd $LATTICE_SRC_PATH
-	lattice_version=$(git describe --always --dirty)
-	mv build/lattice.tgz build/lattice-${lattice_version}.tgz
-popd
+echo $LATTICE_VERSION > $LATTICE_SRC_PATH/build/lattice-build/common/LATTICE_VERSION
+echo $DIEGO_VERSION > $LATTICE_SRC_PATH/build/lattice-build/common/DIEGO_VERSION
+echo $CF_VERSION > $LATTICE_SRC_PATH/build/lattice-build/common/CF_VERSION
 
-ls -l $LATTICE_SRC_PATH/build
+tar -C $LATTICE_SRC_PATH/build czf lattice.tgz lattice-build
+
+mv $LATTICE_SRC_PATH/build/lattice.tgz $LATTICE_SRC_PATH/build/lattice-${lattice_version}.tgz
+
