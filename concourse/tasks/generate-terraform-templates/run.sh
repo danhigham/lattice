cc6d71ec14bfaed3500a66f5c1273d27fd6ccd6b
Stephen Levine <slevine@pivotal.io>
2015-08-03 13:47:32 -0500

    aoeu

--- concourse/tasks/generate-terraform-templates/run.sh
+++ concourse/tasks/generate-terraform-templates/run.sh
@@ -6,10 +6,10 @@ export LATTICE_DIR=$PWD/lattice
 export LATTICE_VERSION=$(git -C $LATTICE_DIR describe)
 
 sed 's/source = "github\.com.*$/source = "github\.com\/cloudfoundry-incubator\/lattice\/\/terraform\/\/aws\?ref='"$LATTICE_VERSION"'"/' \
-	< $LATTICE_DIR/terraform/aws/example > lattice-$LATTICE_VERSION.aws.tf
+	< $LATTICE_DIR/terraform/aws/example/lattice.aws.tf > lattice-$LATTICE_VERSION.aws.tf
 sed 's/source = "github\.com.*$/source = "github\.com\/cloudfoundry-incubator\/lattice\/\/terraform\/\/digitalocean\?ref='"$LATTICE_VERSION"'"/' \
-	< $LATTICE_DIR/terraform/digitalocean/example > lattice-$LATTICE_VERSION.digitalocean.tf
+	< $LATTICE_DIR/terraform/digitalocean/example/lattice.digitalocean.tf > lattice-$LATTICE_VERSION.digitalocean.tf
 sed 's/source = "github\.com.*$/source = "github\.com\/cloudfoundry-incubator\/lattice\/\/terraform\/\/google\?ref='"$LATTICE_VERSION"'"/' \
-	< $LATTICE_DIR/terraform/google/example > lattice-$LATTICE_VERSION.google.tf
+	< $LATTICE_DIR/terraform/google/example/lattice.google.tf > lattice-$LATTICE_VERSION.google.tf
 sed 's/source = "github\.com.*$/source = "github\.com\/cloudfoundry-incubator\/lattice\/\/terraform\/\/openstack\?ref='"$LATTICE_VERSION"'"/' \
-	< $LATTICE_DIR/terraform/openstack/example > lattice-$LATTICE_VERSION.openstack.tf
+	< $LATTICE_DIR/terraform/openstack/example/lattice.openstack.tf > lattice-$LATTICE_VERSION.openstack.tf
