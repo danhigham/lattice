7b611a9c768f94ac3f922242d7f163ec4d730d22
Chris Maher and David Dening <pair+cmaher+ddening@pivotallabs.com>
2015-08-04 12:54:11 -0400

    wip500

--- concourse/tasks/push-lattice-tag/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x -e
-
-export LATTICE_DIR=$PWD/lattice
-export LATTICE_VERSION=$(git -C $LATTICE_DIR describe)
-
-git -C "$LATTICE_DIR" push origin "$LATTICE_VERSION"
--- concourse/tasks/tag-lattice/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x -e
-
-export LATTICE_DIR=$PWD/lattice
-export LATTICE_VERSION=$(cat $LATTICE_DIR/Version)
-
-git -C "$LATTICE_DIR" push origin "$LATTICE_VERSION"
