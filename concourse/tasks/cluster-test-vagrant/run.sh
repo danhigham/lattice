7e217bbef7790b9b577747b86682312de354ca58
Damon Anderson <danderson@pivotal.io>
2015-08-06 16:41:04 -0500

    wip - vagrant up in cluster-test-vagrant

--- concourse/tasks/cluster-test-vagrant/run.sh
+++ concourse/tasks/cluster-test-vagrant/run.sh
@@ -3,4 +3,18 @@
 set -x -e
 
 export LATTICE_DIR=$PWD/lattice
-sleep 10000
+
+#mkdir vagrant-up-tmp
+#cd vagrant-up-tmp
+#
+#curl -LO https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_x86_64.deb
+#dpkg -i vagrant_1.7.4_x86_64.deb
+#
+#vagrant plugin install vagrant-aws
+#
+##XXX# install .pem
+##XXX# AWS ENVS
+#
+#(cp ../lattice-tar-experimental/lattice-*.tgz lattice.tgz && cp ../lattice/Vagrantfile ./ && vagrant up --provider=aws)
+#
+#tar zxf ltc-tar-experimental/ltc-*.tgz && ./ltc-linux-amd64 test -v -t 10m
