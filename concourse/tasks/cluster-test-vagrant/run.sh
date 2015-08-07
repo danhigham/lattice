1e692e8302ce65ba6cd0f9f12ada3e09af39a154
Colin Jackson <colin.s.jackson90@gmail.com>
2015-08-07 08:50:25 -0500

    wip - vagrant up && run integration tests
    Signed-off-by: Damon Anderson <danderson@pivotal.io>

--- concourse/tasks/cluster-test-vagrant/run.sh
+++ concourse/tasks/cluster-test-vagrant/run.sh
@@ -3,18 +3,15 @@
 set -x -e
 
 export LATTICE_DIR=$PWD/lattice
+export AWS_SSH_PRIVATE_KEY_PATH=$PWD/concourse-key/concourse-test.pem
 
-#mkdir vagrant-up-tmp
-#cd vagrant-up-tmp
-#
-#curl -LO https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_x86_64.deb
-#dpkg -i vagrant_1.7.4_x86_64.deb
-#
-#vagrant plugin install vagrant-aws
-#
-##XXX# install .pem
-##XXX# AWS ENVS
-#
-#(cp ../lattice-tar-experimental/lattice-*.tgz lattice.tgz && cp ../lattice/Vagrantfile ./ && vagrant up --provider=aws)
-#
-#tar zxf ltc-tar-experimental/ltc-*.tgz && ./ltc-linux-amd64 test -v -t 10m
+mkdir vagrant-up-tmp
+cd vagrant-up-tmp
+
+curl -LO https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_x86_64.deb && dpkg -i vagrant_1.7.4_x86_64.deb
+
+vagrant plugin install vagrant-aws
+
+(cp ../lattice-tar-experimental/lattice-*.tgz lattice.tgz && cp ../lattice/Vagrantfile ./ && vagrant up --provider=aws)
+
+tar zxf ltc-tar-experimental/ltc-*.tgz && ./ltc-linux-amd64 test -v -t 10m
